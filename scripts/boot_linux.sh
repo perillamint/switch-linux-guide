1989b3ed87cf3bc1d6602d096afe352cb4d9e791
Nicolas Vitaterna <nvitaterna@users.noreply.github.com>
2018-04-27 16:26:41 -0400

    Update boot_linux.sh

--- scripts/boot_linux.sh
+++ scripts/boot_linux.sh
@@ -5,6 +5,6 @@ cd "${0%/*}"
 cd ../bootloader/shofel2/exploit
 ./shofel2.py cbfs.bin ../../coreboot/build/coreboot.rom
 
-../usb_loader
+cd ../usb_loader
 ../../u-boot/tools/mkimage -A arm64 -T script -C none -n "boot.scr" -d switch.scr switch.scr.img
 sudo ../../imx_usb_loader/imx_usb -c .
